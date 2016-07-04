6ebfebac3d05acd1954a289045abbca62329f4e4
FionaLin <clin4614@uni.sydney.edu.au>
2016-07-04 23:20:31 +1000

    session 2  EF Database mapping and facade pattern

---
+++ PersonSolution/Domain/Facade.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Facade
+    {
+        private PersonDBEntities _context = new PersonDBEntities();
+        #region Person
+        public Person FetchPerson(Guid id)
+        {
+            return _context.People.Find(id);
+        }
+        public Person FetchPerson(string name)
+        {
+            return People.Where(p => p.Name.Equals(name)).FirstOrDefault();
+        }
+
+        public List<Person> People
+        {
+            get
+            {
+                return _context.People.ToList();
+            }
+        }
+        public Person AddPerson(string name)
+        {
+            var person = new Person { Name = name };
+            _context.People.Add(person);
+            int result = _context.SaveChanges();
+            return person;
+        }
+        public Person UpdatePerson(Guid id,string name)
+        {
+            var person = _context.People.Find(id);
+            person.Name = name;
+            int result = _context.SaveChanges();
+            return person;
+        }
+        public void DeletePerson(Guid id)
+        {
+            var person = FetchPerson(id);
+            _context.People.Remove(person);
+            _context.SaveChanges();
+        }
+        #endregion
+
+    }
+}
---
+++ PersonSolution/Domain/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var facade = new Facade();
+            //var person = facade.AddPerson("Frank Black");
+
+            //var person = facade.People.Where(p => p.Name.Equals("Frank Black")).First();
+            //var person = facade.FetchPerson("Jack Black");
+            ////person = facade.UpdatePerson(person.Id, "Jack Black");
+            //if(person != null)
+            //{
+            //    person = facade.UpdatePerson(person.Id,"Frank Black");
+            //}
+
+            //delete 
+            var person = facade.FetchPerson("Frank Black");
+            if(person != null)
+            {
+                facade.DeletePerson(person.Id);
+            }
+
+            foreach(var p in facade.People)
+            {
+                Console.WriteLine("Id: {0} Name: {1}", p.Id, p.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
