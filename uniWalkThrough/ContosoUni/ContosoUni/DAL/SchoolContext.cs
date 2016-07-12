df1805b91c08f22c19709bab0d303e1bf6eaae7a
FionaLin <clin4614@uni.sydney.edu.au>
2016-07-13 02:48:18 +1000

    scaffolded student controller and init EF code first clase to create database and test data

---
+++ uniWalkThrough/ContosoUni/ContosoUni/DAL/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUni.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUni.DAL
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext():base("SchoolContext")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            /*table names is pluralized or singular form, it can be whichever form
+             you prefer by including or omitting this line of code*/
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+} 
---
+++ uniWalkThrough/ContosoUni/ContosoUni/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUni.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { set; get; }
+        public string FirstMidName { set; get; }
+        public DateTime EnrollmentDate { set; get; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
